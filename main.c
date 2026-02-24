483a06f842a8fe14274d2717b06775fd5eba5460
Michael Christopher Gubatan Velasquez <mvelasqu@c2r6s13.42singapore.sg>
2026-02-24 12:56:05 +0800

    added strategy for bonus

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mvelasqu <mvelasqu@student.42singapore.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 16:10:09 by mvelasqu          #+#    #+#             */
-/*   Updated: 2026/02/19 13:22:39 by mvelasqu         ###   ########.fr       */
+/*   Updated: 2026/02/24 12:55:53 by mvelasqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,26 @@ Strategy
 				redirect outfile → stdout
 				execve cmd2
 	wait for children
+
+strategy for bonus
+in_fd = open(infile)
+out_fd = open(outfile, TRUNC)
+for each command i = 0..n-1:
+	if i != n-1: pipe(p)
+	pid = fork()
+	child:
+		dup2(in_fd, 0)
+		if i != n-1: dup2(p[1], 1)
+		else: dup2(out_fd, 1)
+		close in_fd
+		if i != n-1: close p[0], close p[1]
+		close out_fd
+		execve(cmd[i])
+	parent:
+		close in_fd
+		if i != n-1:
+		close p[1]
+		in_fd = p[0]
+		close out_fd (and in_fd if still open)
+wait for all children (N times)
 */
